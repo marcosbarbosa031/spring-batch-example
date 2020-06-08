c97e5a468011445d4d7bf122280413816ae17761
Marcos Barbosa <marcosbarbosa031@gmail.com>
2020-06-08 10:58:10 -0300

    First commit

---
+++ src/main/java/com/marcos/barbosa/bachapplication/BachApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.barbosa.bachapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/barbosa/bachapplication/BachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.barbosa.bachapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
